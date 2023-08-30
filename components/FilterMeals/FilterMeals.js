90d8830755df5a39ccd3224d456fa56785161336
Jimmy Zhang <jimmy.zhang@perficient.com>
2023-08-30 16:20:40 -0400

    add asset, store and meals and filter meals components

---
+++ components/FilterMeals/FilterMeals.js
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import classes from "./FilterMeals.module.css";
+
+const FilterMeals = (props) => {
+  const [keyword, setKeyword] = useState("");
+
+  useEffect(() => {
+    //apply filter after user inputs
+    //apply search after one seccond of user inputs
+    const timer = setTimeout(() => {
+      console.log("effect triggers");
+      props.onFilter(keyword);
+    }, 1000);
+
+    //call this before calling useEffect()
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [keyword]);
+
+  const inputChangeHandler = (e) => {
+    // props.onFilter(e.target.value);
+    setKeyword(e.target.value.trim());
+  };
+
+  return (
+    <div className={classes.FilterMeals}>
+      <div className={classes.InputOuter}>
+        <input
+          data-testid="search"
+          value={keyword}
+          onChange={inputChangeHandler}
+          className={classes.SearchInput}
+          type="text"
+          placeholder={"Search Meal..."}
+        />
+        <FontAwesomeIcon className={classes.SearchIcon} icon={faSearch} />
+      </div>
+    </div>
+  );
+};
+
+export default FilterMeals;
---
+++ components/Meals/Meals.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Meal from "./Meal/Meal";
+import classes from "./Meals.module.css";
+
+const Meals = (props) => {
+  return (
+    <div className={classes.Meals} data-testid="meals">
+      {props.mealsData.map((item) => (
+        <Meal key={item.id} meal={item} />
+      ))}
+    </div>
+  );
+};
+
+export default Meals;
