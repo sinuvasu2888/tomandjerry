21556f8c1b9f7734b31c0c2d16caaa7ca619442a
PERFICIENT\srinivas.vudattu <srinivas.vudattu@perficient.com>
2023-08-30 16:14:36 -0400

    adding UI and header components

---
+++ src/components/UI/Backdrop/Backdrop.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import classes from "./Backdrop.module.css";
+
+const backdropRoot = document.getElementById("backdrop-root");
+
+const Backdrop = (props) => {
+  // const ctx = useContext(CartContext);
+
+  return ReactDOM.createPortal(
+    <div {...props} className={`${classes.Backdrop} ${props.className}`}>
+      {props.children}
+    </div>,
+    backdropRoot
+  );
+};
+
+export default Backdrop;
